d3e274db0134af7ab4b3f617db4e55d9edb6e7da
brntava <bruno_tava7@hotmail.com>
2022-05-16 15:11:37 -0300

    eventos de abrir e fechar e dados com formdata

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController{
 
         this.elemenstPrototype();
         this.loadElemenst();
+        this.initEvents();
 
     }
 
@@ -30,10 +31,14 @@ class WhatsAppController{
             return this
         }
 
+        //
+
         Element.prototype.show = function(){
             this.style.display = 'block';
             return this
         }
+
+        //
 
         Element.prototype.toggle = function(){
             this.style.display = (this.style.display == 'none') ? 'block' : 'none';
@@ -62,25 +67,147 @@ class WhatsAppController{
             return this
         }
 
+        //
+
         Element.prototype.addClass = function(name){
             this.classList.add(name);
             return this
         }
+
+        //
 
         Element.prototype.removeClass = function(name){
             this.classList.remove(name);
             return this
         }
 
+        //
+
         Element.prototype.toggleClass = function(name){
             this.classList.toggle(name);
             return this
-        }
+        } 
+
+        //
 
         Element.prototype.hasClass = function(name){
             return this.classList.contains(name);
         }
 
+        // FormData
+
+        HTMLFormElement.prototype.getForm = function(){
+            return new FormData(this)
+        }
+
+        //
+
+        HTMLFormElement.prototype.toJSON = function(){
+            
+            let json = {};
+
+            this.getForm().forEach((value, key) => {
+
+                json[key] = value;
+
+            });
+
+            return json;
+
+        }
+
+    }
+
+    initEvents(){
+
+        this.el.myPhoto.on('click', e =>{
+
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');    
+            }, 100);
+            
+        });
+
+        //
+
+        this.el.btnNewContact.on('click', e =>{
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');    
+            }, 100);
+            
+        });
+
+        //
+
+        this.el.btnClosePanelEditProfile.on('click', e => {
+
+            this.el.panelEditProfile.removeClass('open');
+
+        })
+
+        //
+
+        this.el.btnClosePanelAddContact.on('click', e =>{
+
+            this.el.panelAddContact.removeClass('open');
+
+        })
+
+        //
+
+        this.el.photoContainerEditProfile.on('click', e =>{
+
+            this.el.inputProfilePhoto.click()
+
+        })
+
+        //
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+
+            if(e.key === 'Enter'){
+
+                e.preventDefault();
+
+                this.el.btnSavePanelEditProfile.click();
+
+            }
+
+        })
+
+        //
+        
+        this.el.btnSavePanelEditProfile.on('click', e => {
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML)
+
+        })
+
+        //
+
+        this.el.formPanelAddContact.on('submit', e => {
+
+            e.preventDefault();
+            
+            let formData = new FormData(this.el.formPanelAddContact);
+
+        })
+
+    }
+
+    // Fechar os paineis q ficaram abertos
+
+    closeAllLeftPanel(){
+
+        this.el.panelEditProfile.hide();
+        this.el.panelAddContact.hide();
 
     }
 
